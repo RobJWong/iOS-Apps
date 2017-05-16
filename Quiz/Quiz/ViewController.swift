ab9efaa7d6fb3514d3253876de94459762dc0e74
Robert Wong <RobertJWong246@gmail.com>
2017-05-16 11:18:52 -0400

    init

---
+++ Quiz/Quiz/ViewController.swift
@@ -0,0 +1,51 @@
+//
+//  ViewController.swift
+//  Quiz
+//
+//  Created by Robert Wong on 5/15/17.
+//  Copyright © 2017 Robert Wong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var questionLabel: UILabel!
+    @IBOutlet var answerLabel: UILabel!
+    
+    let questions: [String] = [
+        "What is 7+7?",
+        "What is the capital of Vermont?",
+        "What is cognac made from?"
+    ]
+    
+    let answers: [String] = [
+        "14",
+        "Montpelier",
+        "Grapes"
+    ]
+    
+    var currentQuestionIndex: Int = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        questionLabel.text = questions[currentQuestionIndex]
+    }
+
+    @IBAction func showNextQuestion(_sender: UIButton){
+        currentQuestionIndex += 1
+        if currentQuestionIndex == questions.count {
+            currentQuestionIndex = 0
+        }
+        
+        let question: String = questions[currentQuestionIndex]
+        questionLabel.text = question
+        answerLabel.text = "???"
+    }
+    
+    @IBAction func showAnswer(_sender: UIButton){
+        let answer: String = answers[currentQuestionIndex]
+        answerLabel.text = answer
+    }
+
+}
+
