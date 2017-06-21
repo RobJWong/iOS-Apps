d7082248c19d29d16aefe56ec4baadae3b6d6e1f
Robert Wong <RobertJWong246@gmail.com>
2017-06-21 17:48:55 -0400

    started Homepwner app

---
+++ Homepwner/Homepwner/ItemsViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ItemsViewController.swift
+//  Homepwner
+//
+//  Created by Robert Wong on 6/21/17.
+//  Copyright © 2017 Robert Wong. All rights reserved.
+//
+
+import UIKit
+
+class ItemsViewController: UITableViewController {
+    
+}
